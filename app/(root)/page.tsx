48e77e7a4fb598e606d050fdfff9be08879446c3
p-k-pavan <pkpavan2003@email.com>
2024-10-18 13:01:32 +0530

    sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
-    const loggedIn = {firstName: 'Pavan'};
+    const loggedIn = {firstName: 'Pavan',lastname:'Kumar R',email:'pavan@gmail.com'};
   return (
     <section className="home">
         <div className="home-content">
@@ -20,9 +21,13 @@ const Home = () => {
                   totalBanks={1}
                   totalCurrentBalance={2563.23}
                 />
-                
             </header>
         </div>
+        <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:12356.32},{currentBalance:586235.23}]}
+        />
     </section>
   )
 }
